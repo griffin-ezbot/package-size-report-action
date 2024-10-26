054785980af5e604c0b1ee976e97802a3c890572
Griffin Cox <griffin@ezbot.ai>
2024-10-26 14:03:41 -0500

    Use Working Directory

--- src/lib/npm-ci.js
+++ src/lib/npm-ci.js
@@ -1,13 +1,13 @@
-import fs from 'fs';
-import path from 'path';
-import { rmRF } from '@actions/io';
-import * as log from './log.js';
-import exec from './exec.js';
+import fs from "fs";
+import path from "path";
+import { rmRF } from "@actions/io";
+import * as log from "./log.js";
+import exec from "./exec.js";
 
-async function npmCi({ cwd } = {}) {
-	if (fs.existsSync('node_modules')) {
-		log.info('Cleaning node_modules');
-		await rmRF(path.join(cwd, 'node_modules'));
+async function npmCi({ cwd = process.env.GITHUB_WORKSPACE } = {}) {
+	if (fs.existsSync(path.join(cwd, "node_modules"))) {
+		log.info("Cleaning node_modules");
+		await rmRF(path.join(cwd, "node_modules"));
 	}
 
 	const options = {
@@ -15,24 +15,24 @@ async function npmCi({ cwd } = {}) {
 		ignoreReturnCode: true,
 	};
 
-	let installCommand = '';
+	let installCommand = "";
 
-	if (fs.existsSync('package-lock.json')) {
-		log.info('Installing dependencies with npm');
-		installCommand = 'npm ci';
-	} else if (fs.existsSync('yarn.lock')) {
-		log.info('Installing dependencies with yarn');
+	if (fs.existsSync(path.join(cwd, "package-lock.json"))) {
+		log.info("Installing dependencies with npm");
+		installCommand = "npm ci";
+	} else if (fs.existsSync(path.join(cwd, "yarn.lock"))) {
+		log.info("Installing dependencies with yarn");
 
 		// yarn is installed on GitHub Actions by default
-		installCommand = 'yarn install --frozen-lockfile';
-	} else if (fs.existsSync('pnpm-lock.yaml')) {
-		log.info('Installing dependencies with pnpm');
+		installCommand = "yarn install --frozen-lockfile";
+	} else if (fs.existsSync(path.join(cwd, "pnpm-lock.yaml"))) {
+		log.info("Installing dependencies with pnpm");
 
 		// pnpm is not installed on GitHub Actions by default
-		installCommand = 'npx pnpm i --frozen-lockfile';
+		installCommand = "npx pnpm i --frozen-lockfile";
 	} else {
-		log.info('No lock file detected. Installing dependencies with npm');
-		installCommand = 'npm i';
+		log.info("No lock file detected. Installing dependencies with npm");
+		installCommand = "npm i";
 	}
 
 	const { exitCode, stdout, stderr } = await exec(installCommand, options);
