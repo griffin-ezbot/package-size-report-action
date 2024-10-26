abc974c6295348c1833bc2cb795b2e3af19c1b0c
Griffin Cox <griffin@ezbot.ai>
2024-10-26 14:28:58 -0500

    use process

--- src/lib/build-ref.js
+++ src/lib/build-ref.js
@@ -1,18 +1,14 @@
-import fs from 'fs';
-import * as log from './log.js';
-import exec from './exec.js';
-import npmCi from './npm-ci.js';
-import isFileTracked from './is-file-tracked.js';
-import { c, link } from './markdown.js';
+import fs from "fs";
+import * as log from "./log.js";
+import exec from "./exec.js";
+import npmCi from "./npm-ci.js";
+import isFileTracked from "./is-file-tracked.js";
+import { c, link } from "./markdown.js";
 
 let pkgSizeInstalled = false;
 
-async function buildRef({
-	checkoutRef,
-	refData,
-	buildCommand,
-}) {
-	const cwd = process.cwd();
+async function buildRef({ checkoutRef, refData, buildCommand }) {
+	const cwd = process.env.GITHUB_WORKSPACE;
 
 	log.info(`Current working directory: ${cwd}`);
 
@@ -30,18 +26,18 @@ async function buildRef({
 		// log.info('Changed working directory', cwd);
 	}
 
-	if (buildCommand !== 'false') {
+	if (buildCommand !== "false") {
 		if (!buildCommand) {
 			let pkgJson;
 			try {
-				pkgJson = JSON.parse(fs.readFileSync('./package.json'));
+				pkgJson = JSON.parse(fs.readFileSync("./package.json"));
 			} catch (error) {
-				log.warning('Error reading package.json', error);
+				log.warning("Error reading package.json", error);
 			}
 
 			if (pkgJson && pkgJson.scripts && pkgJson.scripts.build) {
-				log.info('Build script found in package.json');
-				buildCommand = 'npm run build';
+				log.info("Build script found in package.json");
+				buildCommand = "npm run build";
 			}
 		}
 
@@ -53,20 +49,22 @@ async function buildRef({
 			log.info(`Running build command: ${buildCommand}`);
 			const buildStart = Date.now();
 			await exec(buildCommand, { cwd }).catch((error) => {
-				throw new Error(`Failed to run build command: ${buildCommand}\n${error.message}`);
+				throw new Error(
+					`Failed to run build command: ${buildCommand}\n${error.message}`
+				);
 			});
 			log.info(`Build completed in ${(Date.now() - buildStart) / 1000}s`);
 		}
 	}
 
 	if (!pkgSizeInstalled) {
-		log.info('Installing pkg-size globally');
-		await exec('npm i -g pkg-size');
+		log.info("Installing pkg-size globally");
+		await exec("npm i -g pkg-size");
 		pkgSizeInstalled = true;
 	}
 
-	log.info('Getting package size');
-	const result = await exec('pkg-size --json', { cwd }).catch((error) => {
+	log.info("Getting package size");
+	const result = await exec("pkg-size --json", { cwd }).catch((error) => {
 		throw new Error(`Failed to determine package size: ${error.message}`);
 	});
 	log.debug(JSON.stringify(result, null, 4));
@@ -79,23 +77,26 @@ async function buildRef({
 		sizeBrotli: 0,
 	};
 
-	await Promise.all(pkgData.files.map(async (file) => {
-		pkgData.size += file.size;
-		pkgData.sizeGzip += file.sizeGzip;
-		pkgData.sizeBrotli += file.sizeBrotli;
+	await Promise.all(
+		pkgData.files.map(async (file) => {
+			pkgData.size += file.size;
+			pkgData.sizeGzip += file.sizeGzip;
+			pkgData.sizeBrotli += file.sizeBrotli;
 
-		const isTracked = await isFileTracked(file.path);
-		file.isTracked = isTracked;
-		file.label = (
-			isTracked
-				? link(c(file.path), `${refData.repo.html_url}/blob/${refData.ref}/${file.path}`)
-				: c(file.path)
-		);
-	}));
+			const isTracked = await isFileTracked(file.path);
+			file.isTracked = isTracked;
+			file.label = isTracked
+				? link(
+						c(file.path),
+						`${refData.repo.html_url}/blob/${refData.ref}/${file.path}`
+				  )
+				: c(file.path);
+		})
+	);
 
-	log.info('Cleaning up');
-	await exec('git reset --hard'); // Reverts changed files
-	const { stdout: cleanList } = await exec('git clean -dfx'); // Deletes untracked & ignored files
+	log.info("Cleaning up");
+	await exec("git reset --hard"); // Reverts changed files
+	const { stdout: cleanList } = await exec("git clean -dfx"); // Deletes untracked & ignored files
 	log.debug(cleanList);
 
 	return pkgData;
